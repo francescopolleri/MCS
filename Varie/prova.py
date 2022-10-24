20d5d6d9c7ed8abe7b16fd49c11a9e4a0bd7a137
francescopolleri <frapolleri@gmail.com>
2022-10-24 11:04:46 +0200

    varie py

---
+++ Varie/prova.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+print("Hello World")
+
+l=[1, 2, 3, 4, 5, 6]
+
+print(l[2:5])
+
+thing=[23,45]
+
+thing.append('cat')
+
+thing[1]='dog'
+
+print(thing)
+
+a=2
+if a==1:
+    print(a)
+elif a==2:
+    print(a)
+    print("a vale 2")
+
+for word in ('qwerty','uiop','asdf'):
+    print('%s' %word)
+
