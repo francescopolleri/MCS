976838c4eff312986bbefd8dad69ef72a202b94b
francescopolleri <frapolleri@gmail.com>
2022-11-12 09:36:20 +0100

    prova residui

--- Varie/guadagnoDIFF.C
+++ Varie/guadagnoDIFF.C
@@ -26,11 +26,12 @@ TGraphErrors* g1 = new TGraphErrors;
 TF1* f = new TF1("f","[0]+x*[1]");
 
 vector<double> vi;
-vector<double> vo;
+vector<double> vo,evi,evo;
+
 for(int i=0; file>>Vin>>fsVin>>Vout>>fsVout; i++){
     vi.push_back(Vin);
     vo.push_back(Vout);
- double eVin, eVout;
+    double eVin, eVout;
     if (fsVin<0.01){
         eVin=get_VRangeErr(0.04,8,fsVin)/sqrt(3);
         }
@@ -43,20 +44,21 @@ for(int i=0; file>>Vin>>fsVin>>Vout>>fsVout; i++){
     else{
         eVout=get_VRangeErr(0.03,8,fsVout)/sqrt(3);
     }
-g1->SetPoint(i,Vin,Vout);
-g1->SetPointError(i,eVin,eVout);
+    evo.push_back(eVout);
+    evi.push_back(eVin);
+    g1->SetPoint(i,Vin,Vout);
+    g1->SetPointError(i,eVin,eVout);
 
 }
 
 TCanvas* c1 = new TCanvas("","",800,600);
-//c.Divide(2,1,0.01,0.01,0);
-c1->cd();
+c1->Divide(1,2,0.01,0.01,0);
+c1->cd(1);
 
 g1->Fit("f");
 g1->Draw("ap");
 g1->SetTitle("Fit G_DIFF;V_in[V];V_out[V]");
 gStyle->SetOptFit();
-
 
 double q = f->GetParameter(0);
 double G = f->GetParameter(1);
@@ -64,21 +66,17 @@ double G = f->GetParameter(1);
 double eq = f->GetParError(0);
 double eG = f->GetParError(1);
 
-TCanvas* c3 = new TCanvas("","",800,600);
-c3->cd();
+c1->cd(2);
 TGraphErrors *g3 = new TGraphErrors;
-TF1 *h = new TF1("h","sin(x)",0,10);
-h->Draw();
+TF1 *h = new TF1("h","0*x",0,1);
 
-vector<double> fv; 
 for(int i=0;i<vi.size();i++){
-   //fv.push_back(f.Eval(v[i]));
-   g3->SetPoint(i,vi[i],f->Eval(vi[i])-vo[i]);
-   g3->SetPointError(i,0.001,0,001)
-   g3->SetMarkerSize(100);
+   g3->SetPoint(i,vi[i],(vo[i]-f->Eval(vi[i]))/evo[i]);
+   g3->SetPointError(i,evi[i], sqrt(pow(evi[i],2)+pow(evo[i],2)) );  //definire meglio gli errori !!!
 }
 
 g3->Draw("ap");
+h->Draw("same");
 
 std::cout<<"quota = "<<q<<" +/- "<<eq<<std::endl;
 std::cout<<"G_DIFF = "<<G<<" +/- "<<eG<<std::endl;
