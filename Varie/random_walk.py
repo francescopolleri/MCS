5c218f3c49ad634e95bed4d1ac519c2b2e555ce8
francescopolleri <frapolleri@gmail.com>
2022-12-09 11:15:24 +0100

    random walk

--- Varie/random_walk.py
+++ Varie/random_walk.py
@@ -4,12 +4,51 @@ import math as m
 rnd=TRandom3()
 rnd.SetSeed(123456789)
 
-d=m.sqrt(3)
 f=0.5
-N=100000
+nev=[20, 40, 60]
+N=1000
 
-for i in range (0,N):
-    dx=d*rnd.Rndm()
-    sx=-d*rnd.Rndm()
+c1=TCanvas("c1","",500,500)
+c2=TCanvas("c2","",500,500)
+c3=TCanvas("c3","",500,500)
+g=TGraph()
+h20=TH1D("h20","",100,0.,0.)
+h40=TH1D("h40","",100,0.,0.)
+h60=TH1D("h60","",100,0.,0.)
+
+s=0
+s2=0
+for j in range(len(nev)):
+    N=nev(j)
+    for i in range (0,N):
+        if rnd.Rndm()<f:
+            d=rnd.Rndm()*m.sqrt(3)
+        else:
+            d= -rnd.Rndm()*m.sqrt(3)
+    
+        s = s+d
+    
+
+c1.cd()
+h1.Draw()
+c2.cd()
+h2.Draw()
+c3.cd()
+h3.Draw()
+    
+
+    #s2 += pow(s,2)
+    #g.SetPoint(i,i,s2)
+
+#c1.cd()
+#g.Draw("AP")
+
+gApplication.Run(True)
+
+
+    
+
+
+
 
     
