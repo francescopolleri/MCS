4c35c4eaff6e54dfec6b856851bdb2fb21bff34a
francescopolleri <frapolleri@gmail.com>
2022-12-18 18:07:41 +0100

    cazzate...

--- Varie/es4_esame21_1_19.C
+++ Varie/es4_esame21_1_19.C
@@ -3,7 +3,7 @@ using namespace std;
 void es4_esame21_1_19(){
     ifstream file("dati_gamma.txt");
     double x;
-    TH1D *h = new TH1D("h","",50,0.,0.);
+    TH1D *h = new TH1D("h","",100,0,30);
     while (file>>x){
         h->Fill(x);
     }
@@ -11,18 +11,16 @@ void es4_esame21_1_19(){
     TTree *t = new TTree();
     t->ReadFile("dati_gamma.txt","t/D");
 
-    TF1 *f = new TF1("f","[0]*pow(x,2)*exp(-x/[1])/(2*pow([1],3))",0,26);
-    f->SetParameter(0,1.);
+    TF1 *f = new TF1("f","[0]*pow(x,2)*exp(-x/[1])/(2*pow([1],3))",0,20);
+    f->FixParameter(0,1.);
     f->SetParameter(1,2.);
-    f->SetParLimits(0,0,10);
-    f->SetParLimits(1,1,3);
 
-    t->UnbinnedFit("f","t","t<17");
+    t->UnbinnedFit("f","t");
+    
+     h->SetMarkerStyle(20);
+     h->Draw("E");
 
-    f->SetParameter(1,h->GetEntries()*h->GetBinWidth(1));
-
-    h->SetMarkerStyle(20);
-    h->Draw("E");
+    f->SetParameter(0,h->GetEntries()*h->GetBinWidth(1));
     f->Draw("same");
 
 }
