b06f3c9c3c3f28e42f1141116a5c4998c75d46b1
francescopolleri <frapolleri@gmail.com>
2022-12-18 17:13:43 +0100

    fanculopdssoofnvof

--- Varie/es4_esame21_1_19.C
+++ Varie/es4_esame21_1_19.C
@@ -1,26 +1,30 @@
 using namespace std;
 
 void es4_esame21_1_19(){
-    ifstream file("dati_gamma.dat");
+    ifstream file("dati_gamma.txt");
     double x;
-    TH1D *h=new TH1D("h","",100,0.,0.);
+    TH1D *h = new TH1D("h","",50,0.,0.);
     while (file>>x){
         h->Fill(x);
     }
 
-    TTree *t= new TTree();
-    t->ReadFile("dati_gamma.dat","t/D");
+    TTree *t = new TTree();
+    t->ReadFile("dati_gamma.txt","t/D");
 
-    TF1 *f=new TF1("f","[1]*(pow(x,2)*exp(-x/[0]))/(2*pow([0],3))",0,30);
-        
-    f->SetParameter(0,1.5);
-    f->FixParameter(1,1.);
+    TF1 *f = new TF1("f","[0]*pow(x,2)*exp(-x/[1])/(2*pow([1],3))",0,26);
+    f->SetParameter(0,1.);
+    f->SetParameter(1,2.);
+    f->SetParLimits(0,0,10);
+    f->SetParLimits(1,1,3);
 
-    t->UnbinnedFit("f","t");
-
-    h->Draw();
+    t->UnbinnedFit("f","t","t<17");
 
     f->SetParameter(1,h->GetEntries()*h->GetBinWidth(1));
-    f->Draw("SAME");
+
+    h->SetMarkerStyle(20);
+    h->Draw("E");
+    f->Draw("same");
 
 }
+
+
