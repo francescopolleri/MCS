dca5ed00af51807488356d3ed4784352e57dccc8
francescopolleri <frapolleri@gmail.com>
2022-12-25 09:29:50 +0100

    pneriqvè

--- Hands_on4/unb_fit.C
+++ Hands_on4/unb_fit.C
@@ -7,20 +7,29 @@ TH1D *h=new TH1D("h","",50,0,58);
 
 tree->ReadFile("dati_highstat.dat","t/D");
 
-TF1 *f=new TF1("f","[0]*((1-[1])*TMath::Gaus(x,[2],[3]) + [1]*exp(-x/[4])/[4])");
-f->FixParameter(0,1);
-f->SetParameter(1,h->GetEntries()*h->GetBinWidth(2));
+TF1 *f=new TF1("f","[0]*([1]*TMath::Gaus(x,[2],[3]) + (1-[1])*exp(-x/[4])/[4])");
+f->FixParameter(0,1040*1.16);
+//f->SetParameter(1,h->GetEntries()*h->GetBinWidth(2));
 f->SetParameter(2,15);
 f->SetParameter(4,10);
+f->SetParameter(3,0.5);
+
+f->SetParLimits(2,14,16);
+f->SetParLimits(4,8,12);
+//f->SetParLimits(1,0,10);
+f->SetParLimits(3,0.1,2);
 
 ifstream file("dati_highstat.dat");
 double y;
 while(file>>y){
     h->Fill(y);
 }
-h->Draw();
 
 tree->UnbinnedFit("f","t");
+//h->Fit("f","L");
+h->Draw();
+f->Draw("same");
+
 
 
 }
--- Varie/Esercizi_esami/es3_esami.cpp
+++ Varie/Esercizi_esami/es3_esami.cpp
@@ -13,10 +13,8 @@ int main(){
 TApplication app("app",NULL,NULL);
 
 TH1D *h=new TH1D("h","",30,0.55,0.6);
-TH1D *k=new TH1D("k","",30,0.,0.);
 
 TCanvas *c1=new TCanvas("c1");
-TCanvas *c2=new TCanvas("c2");
 
 ifstream file("Dati_spettrometro.txt");
 double x;
@@ -26,27 +24,11 @@ while(file>>x){
     h->Fill(x);   
 }
 file.close();
-c1->cd();
 h->Draw();
 
 double area=500*h->GetBinWidth(2);
-ifstream ifile("Dati_spettrometro.txt");
-double y;
-while(ifile>>y){
-    k->Fill(y/area);
-}
-c2->cd();
-//k->Draw();
 
-TF1 *f=new TF1("f","TMath::Gaus(x,[0],[1],1)+(1-[0])*(TMath::Gaus(x,[2],[3],1))");
 
-f->SetParameter(1,0.681);
-f->SetParameter(3,0.702);
-f->SetParameter(2,0.005);
-f->SetParameter(4,0.001);
-
-k->Fit("f","MULTI");
-k->Draw();
 
 app.Run(true);
 
