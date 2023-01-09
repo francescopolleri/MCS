31f274548b74dcdf68b2b13ae369087adced5b85
francescopolleri <frapolleri@gmail.com>
2023-01-09 19:39:34 +0100

    varie...

--- Varie/random_walk.py
+++ Varie/random_walk.py
@@ -1,47 +1,74 @@
+import numpy as np 
+import matplotlib.pyplot as plt
 from ROOT import *
-import math as m
 
 rnd=TRandom3()
-rnd.SetSeed(123456789)
+rnd.SetSeed(1324758609)
 
-f=0.5
-nev=[20, 40, 60]
-N=1000
+Nev=1000
 
-c1=TCanvas("c1","",500,500)
-c2=TCanvas("c2","",500,500)
-c3=TCanvas("c3","",500,500)
-g=TGraph()
-h20=TH1D("h20","",100,0.,0.)
-h40=TH1D("h40","",100,0.,0.)
-h60=TH1D("h60","",100,0.,0.)
+N_urti=20
+x=0; d=0; x2=0
+s=np.zeros([])
+s2=np.zeros([])
+n_urti=np.zeros([])
+for j in range (0,N_urti):
+    n_urti=np.append(n_urti,j)
+    for i in range (0,Nev):
+        d=2*np.sqrt(3)*rnd.Rndm()-np.sqrt(3)
+    x += d
+    x2 += d**2
+    s=np.append(s,x)
+    s2=np.append(s2,x2)
+plt.plot(n_urti,s2,"-g",label="#Urti=20")
 
-s=0
-s2=0
-for j in range(len(nev)):
-    N=nev(j)
-    for i in range (0,N):
-        if rnd.Rndm()<f:
-            d=rnd.Rndm()*m.sqrt(3)
-        else:
-            d= -rnd.Rndm()*m.sqrt(3)
-    
-        s = s+d
-    
+N_urti=40
+x=0; d=0; x2=0
+s=np.zeros([])
+s2=np.zeros([])
+n_urti=np.zeros([])
+for j in range (0,N_urti):
+    n_urti=np.append(n_urti,j)
+    for i in range (0,Nev):
+        d=2*np.sqrt(3)*rnd.Rndm()-np.sqrt(3)
+    x += d
+    x2 += d**2
+    s=np.append(s,x)
+    s2=np.append(s2,x2)
+plt.plot(n_urti,s2,"-b",label="#Urti=40")
 
-c1.cd()
+N_urti=60
+x=0; d=0; x2=0
+s=np.zeros([])
+s2=np.zeros([])
+n_urti=np.zeros([])
+for j in range (0,N_urti):
+    n_urti=np.append(n_urti,j)
+    for i in range (0,Nev):
+        d=2*np.sqrt(3)*rnd.Rndm()-np.sqrt(3)
+    x += d
+    x2 += d**2
+    s=np.append(s,x)
+    s2=np.append(s2,x2)
+plt.plot(n_urti,s2,"-r",label="#Urti=60")
+
+
+x=0
+d=0
+h1=TH1D("h","",15,0.,0.)  
+N_particles=1000
+N_step=100
+for k in range (0,N_particles):
+    x=0
+    for j in range (0,N_step):
+        for i in range (0,Nev):
+            d=2*np.sqrt(3)*rnd.Rndm()-np.sqrt(3)
+        x += d
+    h1.Fill(x)
+
+c1=TCanvas("c1","",400,400)
+c1.Draw()
 h1.Draw()
-c2.cd()
-h2.Draw()
-c3.cd()
-h3.Draw()
-    
-
-    #s2 += pow(s,2)
-    #g.SetPoint(i,i,s2)
-
-#c1.cd()
-#g.Draw("AP")
 
 gApplication.Run(True)
 
